473b3fed59ed7647cac3382fb2adc851edbb333f
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-03-21 19:21:48 +0530

    Managing state using custom hooks based store

---
+++ src/hooks-store/store.js
@@ -0,0 +1,38 @@
+import {useState, useEffect} from 'react';
+
+let globalState = {};
+let listeners = [];
+let actions = {};
+
+export const useStore = () => {
+    const setState = useState(globalState)[1];
+    const dispatch = (actionIdentifer, payload) => {
+        const newState = actions[actionIdentifer](globalState, payload);
+        globalState = {
+            ...globalState,
+            ...newState
+        }
+
+        for(const listener of listeners ) {
+            listener(globalState);
+        }
+    }
+
+    useEffect(() => {
+        listeners.push(setState);
+
+        return (() => {
+            listeners = listeners.filter(li => li !== setState);
+        });
+    }, [setState]);
+
+    return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+    if(initialState) {
+        globalState = {...globalState, ...initialState};
+    }
+
+    actions = {...actions, ...userActions};
+}
